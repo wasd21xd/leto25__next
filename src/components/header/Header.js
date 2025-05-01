cbeb147fb8a33d5904e596217d60d328d012a818
wasd21xd <Avey21703@gmail.com>
2025-05-01 21:57:39 +0300

    header11

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,36 @@
-import 'src/components/header/header.css'
+import React from 'react';
+// Импортируем CSS модуль вместо SCSS
+import './header.css';
 
-export default function Header() {
+const Header = () => {
     return (
-    <div></div>
+        // Используем классы из CSS модуля
+        <header className='header'>
+            <div className='container'>
+                <div className='leftColumn'>
+                    <a href="#" className='ctaButton'>
+                        Забронировать
+                    </a>
+                    <p className='discountText'>
+                        прямо сейчас и получить скидку
+                    </p>
+                </div>
+                <div className='rightColumn'>
+                    <h1 className='mainHeading'>
+                        Каникулы <br /> в городе <br /> с{' '}
+                        <span className='highlight'>Uchi.ru</span>
+                    </h1>
+                    <h2 className='subHeading'>Полный день:</h2>
+                    <ul className='featuresList'>
+                        <li className='featureItem'>Программирование</li>
+                        <li className='featureItem'>Творческие мастер-классы</li>
+                        <li className='featureItem'>Прогулки на свежем воздухе</li>
+                        <li className='featureItem'>и другое!</li>
+                    </ul>
+                </div>
+            </div>
+        </header>
     );
-}
+};
 
+export default Header;
